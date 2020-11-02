22e2be46aa62e76327c59e371d507a5c98a69bd7
drimix001 <69410679+drimix001@users.noreply.github.com>
2020-11-02 21:53:11 +0200

    Updates

---
+++ precache-manifest.55571198d812c49c9e18ee6752da6fb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcda4da9cb88a31b71f21ac9ba20da87",
+    "url": "/mine/index.html"
+  },
+  {
+    "revision": "7d0bb77a0edd9f3487aa",
+    "url": "/mine/static/css/main.9e031ee4.chunk.css"
+  },
+  {
+    "revision": "2eebba7d16c43eadbcd7",
+    "url": "/mine/static/js/2.15616bcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mine/static/js/2.15616bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0bb77a0edd9f3487aa",
+    "url": "/mine/static/js/main.7bf108d9.chunk.js"
+  },
+  {
+    "revision": "e734ac2866e8d333a8fb",
+    "url": "/mine/static/js/runtime-main.db0e7b7f.js"
+  }
+]);
